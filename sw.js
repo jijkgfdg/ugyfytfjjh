25afac28b5cd70e77dadaddacb37502b1c3630e8
jijkgfdg <274004403+jijkgfdg@users.noreply.github.com>
2026-04-06 21:16:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,20 @@
-const CACHE_NAME = 'mahjong-score-pwa-v1';
-const URLS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
+const CACHE_NAME = 'mahjong-pwa-v2';
+const URLS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        const cloned = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
   );
 });
